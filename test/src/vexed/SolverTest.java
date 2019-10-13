eedf8de0f5ea7bfe598d5551a0d64f7638311d68
Josh Joyce <j_joyce2001@yahoo.com>
2019-10-13 18:49:10 -0500

    use var

--- test/src/vexed/SolverTest.java
+++ test/src/vexed/SolverTest.java
@@ -22,23 +22,23 @@ public abstract class SolverTest {
 
     @Test
     public void solveTrivialBoard() {
-        MapBoard.Builder boardBuilder = new MapBoard.Builder(3);
+        var boardBuilder = new MapBoard.Builder(3);
         boardBuilder.addInteriorRow(" A ");
         boardBuilder.addInteriorRow(" # ");
         boardBuilder.addInteriorRow("A  ");
         Board board = boardBuilder.build(_positionSupplier);
-        MoveHistory moves = _solver.solve(board).getMoveHistory();
+        var moves = _solver.solve(board).getMoveHistory();
         assertEquals(Collections.singletonList(new Move(2, 0, Direction.Left)), moves.getMoves());
     }
 
     @Test
     public void solveNonTrivialBoard() {
-        MapBoard.Builder boardBuilder = new MapBoard.Builder(5);
+        var boardBuilder = new MapBoard.Builder(5);
         boardBuilder.addInteriorRow(" BA  ");
         boardBuilder.addInteriorRow(" ##  ");
         boardBuilder.addInteriorRow("  A B");
         Board board = boardBuilder.build(_positionSupplier);
-        MoveHistory moves = _solver.solve(board).getMoveHistory();
+        var moves = _solver.solve(board).getMoveHistory();
         assertEquals(Arrays.asList(new Move(3, 0, Direction.Right),
                 new Move(2, 0, Direction.Right),
                 new Move(3, 0, Direction.Right)), moves.getMoves());
@@ -46,7 +46,7 @@ public abstract class SolverTest {
 
     @Test
     public void solveHellishBoard() {
-        MapBoard.Builder boardBuilder = new MapBoard.Builder(6);
+        var boardBuilder = new MapBoard.Builder(6);
         boardBuilder.addInteriorRow("   C  ");
         boardBuilder.addInteriorRow("  BA  ");
         boardBuilder.addInteriorRow("  ##  ");
@@ -57,7 +57,7 @@ public abstract class SolverTest {
 
     @Test
     public void solveEvilBoard() {
-        MapBoard.Builder builder = new MapBoard.Builder(5);
+        var builder = new MapBoard.Builder(5);
         builder.addInteriorRow("  Y  ");
         builder.addInteriorRow(" ZX X");
         builder.addInteriorRow(" ## #");
@@ -68,8 +68,23 @@ public abstract class SolverTest {
     }
 
     @Test
+    public void solveBenchmarkBoard() {
+        var builder = new MapBoard.Builder(8);
+        builder.addInteriorRow("D       ");
+        builder.addInteriorRow("#       ");
+        builder.addInteriorRow("   A    ");
+        builder.addInteriorRow("   H#   ");
+        builder.addInteriorRow("   C ## ");
+        builder.addInteriorRow("   #    ");
+        builder.addInteriorRow(" # #    ");
+        builder.addInteriorRow("H  D# AC");
+        var board = builder.build(_positionSupplier);
+        System.out.println(_solver.solve(board));
+    }
+
+    @Test
     public void recognizeImpossibleToSolveBoard() {
-        MapBoard board = MapBoard.fromString("#D#", _positionSupplier);
+        var board = MapBoard.fromString("#D#", _positionSupplier);
         try {
             _solver.solve(board);
             fail("expected exception");
