a278c165f713f2261e05cf08978a7f26ef49195b
Josh Joyce <jnjoyce@pobox.com>
2020-07-25 00:54:54 -0500

    use computeIfAbsent

--- src/vexed/CachingPositionSupplier.java
+++ src/vexed/CachingPositionSupplier.java
@@ -5,33 +5,11 @@ import java.util.Map;
 
 public class CachingPositionSupplier implements PositionSupplier {
 
-    private final Map<Integer, Map<Integer, Position>> _cache = new HashMap<>();
+    private final Map<Integer, Map<Integer, Position>> cache = new HashMap<>();
 
     @Override
     public Position getPosition(int row, int col) {
-        if (_cache.containsKey(row)) {
-            return getFromSubCache(row, col);
-        } else {
-            return addSubCache(row, col);
-        }
-    }
-
-    private Position addSubCache(int row, int col) {
-        Map<Integer, Position> subCache = new HashMap<>();
-        Position position = new Position(col, row);
-        subCache.put(col, position);
-        _cache.put(row, subCache);
-        return position;
-    }
-
-    private Position getFromSubCache(int row, int col) {
-        Map<Integer, Position> subCache = _cache.get(row);
-        if (subCache.containsKey(col)) {
-            return subCache.get(col);
-        } else {
-            Position position = new Position(col, row);
-            subCache.put(col, position);
-            return position;
-        }
+        final var rowCache = cache.computeIfAbsent(row, r -> new HashMap<>());
+        return rowCache.computeIfAbsent(col, c -> new Position(col, row));
     }
 }
